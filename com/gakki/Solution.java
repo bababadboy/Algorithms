de9ba78ccd16bd6293c0e0748764e99024886086
bababadboy <sc30logo@gmail.com>
2018-12-16 17:35:31 +0800

    First commit “无重复最长子串”

--- com/gakki/Solution.java
+++ com/gakki/Solution.java
@@ -1,7 +1,35 @@
 package com.gakki;
 
+import java.util.HashMap;
+
 /**
  * @author wangxiaobin
  */
 public class Solution {
+
+    /**
+     * 寻找最长无重复子串长度
+     * @param s 字符串
+     * @return 最长无重复子串长度
+     */
+    public static int lengthOfLongestSubstring(String s){
+        int len = 0;
+        int head = 0;
+        HashMap<Character,Integer> hashMap = new HashMap<>(); // <字符，位置>
+
+        for (int i = 0; i < s.length(); i++){
+            if (!hashMap.containsKey(s.charAt(i))){ // 不包含重复的键
+                hashMap.put(s.charAt(i),i);
+                if (i - head + 1 > len) {
+                    len = i - head + 1;
+                }
+
+            }
+            else { // 出现了重复字符
+                head = hashMap.get(s.charAt(i)) + 1; // 获得重复字符的位置 + 1
+                hashMap.put(s.charAt(i),i);
+            }
+        }
+        return len;
+    }
 }
