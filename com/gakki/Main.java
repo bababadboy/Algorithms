c57535c5ae794b4ae621459f654c15c6a1884c3e
bababadboy <sc30logo@gmail.com>
2019-02-12 20:24:43 +0800

    Temple commit

--- com/gakki/Main.java
+++ com/gakki/Main.java
@@ -1,19 +1,25 @@
 package com.gakki;
 
+import com.gakki.chapter2.MaxPQ;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.print("helloworld");
-	    int N = 10;
-	    double a[]= new double[N];
-//	    a[0] = 1;
-//	    for (int i = 0; i < 10; i++)
-//			a[i] = 0;
-//	    a = {1,2,3,4,5,6,7,8,9,10};
-		for (int i = 0; i < 10; i ++)
-	    	System.out.print(a[i]+"\t");
-		double b = Math.exp(a[0]);
-		System.out.print("\nb="+b);
+        /* 给定大小构造函数 */
+        int size = 10;
+        MaxPQ<Integer> maxPQ = new MaxPQ<>(size);
+        for (int i = 0; i < size; i++) {
+            maxPQ.insert(i);
+        }
+        maxPQ.print();
 
-	}
+        for (int i = 0; i < size; i++) {
+            System.out.print(maxPQ.delMax() + "\t");
+        }
+
+        /* 传入数组的构造函数 */
+        String[] strings = new String[]{"AI", "Lebron", "WANG", "JIM", "Stephen", "Rose"};
+        MaxPQ<String> maxPQ1 = new MaxPQ<>(strings);
+        maxPQ1.print();
+    }
 }
