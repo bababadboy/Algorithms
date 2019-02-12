7cc25818091b7a6219d7703befe88bb3d2f4331a
bababadboy <sc30logo@gmail.com>
2019-02-12 22:49:48 +0800

    修改less防范outofbound的bug，原因是第二层for循环有错。

--- com/gakki/chapter2/one_primarySorting/Shell.java
+++ com/gakki/chapter2/one_primarySorting/Shell.java
@@ -9,10 +9,10 @@ public class Shell {
         int N = a.length;
         int h = 1;  // 初始化h序列，h的值
         while (h < N/3) h = h*3+1;    // 找到合适大小的h值，除号类似 i < n-1 中的减号
-        while (h > 1) {
+        while (h >= 1) {
             for (int i = h; i < N; i++) {   // 从a[]的第h个元素开始，a[h],a[h+1],a[h+2]..a[N-1]
-                for (int j = i; less(a,j,j-h) && j > 1; j -= h) {   // exchange高端小的元素到低端，增量为h
-                    exch(a,i,j);
+                for (int j = i;j >= h && less(a,j,j-h); j -= h) {   // exchange高端小的元素到低端，增量为h
+                    exch(a,j,j-h);
                 }
             }
             h = h /3;
@@ -31,6 +31,8 @@ public class Shell {
     public static void main(String[] args) {
         String s[] = {"S","H","E","L","L","S","O","R","T","E","X","A","M","P","L","E"};
         sort(s);
-        System.out.print(s);
+        for (int i = 0; i < s.length; i ++){
+            System.out.print(s[i]);
+        }
     }
 }
