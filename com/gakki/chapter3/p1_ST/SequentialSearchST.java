a0ba615f2c9ee72d64bc0a95ae3b16b84e776a22
bababadboy <sc30logo@gmail.com>
2019-02-18 15:59:12 +0800

    将有序链表修改为无序链表，并修改若干bugs

--- com/gakki/chapter3/p1_ST/SequentialSearchST.java
+++ com/gakki/chapter3/p1_ST/SequentialSearchST.java
@@ -7,11 +7,11 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 /**
- * 顺序查找（基于有序链表）
+ * 顺序查找（基于无序链表）
  * @author wangxiaobin
  */
 public class SequentialSearchST<Key extends Comparable<Key>,Value>{
-    private Node head;
+    private Node head;  // 第一个节点
     private int length;
     private class Node{
         Key key;
@@ -20,23 +20,25 @@ public class SequentialSearchST<Key extends Comparable<Key>,Value>{
     }
 
     public SequentialSearchST() {
-        head = new Node();
-        head.next = null;
+        head = null;
         length = 0;
     }
-
+    public int size(){
+        return length;
+    }
     public Iterable<Key> keys() {
         Queue<Key> queue = new PriorityQueue<>();   // 使用java.util中的优先队列
-        Node p = head.next;
+        Node p = head;
         while (p != null) {
             queue.add(p.key);
+            p = p.next;
         }
         return queue;
     }
 
     public boolean contains(Key k) {
         boolean ifContains = false;
-        Node p = head.next;
+        Node p = head;
         while (p != null) {
             if (p.key.compareTo(k) == 0) {
                 ifContains = true;
@@ -48,7 +50,7 @@ public class SequentialSearchST<Key extends Comparable<Key>,Value>{
     }
 
     public Value get(Key k) {
-        Node p = head.next;
+        Node p = head;
         Value val = null;   // 返回的值
         while (p != null) {
             if (p.key.compareTo(k) == 0) {
@@ -61,25 +63,22 @@ public class SequentialSearchST<Key extends Comparable<Key>,Value>{
     }
 
     public void put(Key k, Value v){
-
-        // 找到待插入的位置
+        // 如果有相同的key，则更新val
         Node p = head;
-        while (p.next != null && less(p.next.key,k)) {
+        while (p != null) {
+            if (k == p.key) {
+                p.val = v;
+                return;
+            }
             p = p.next;
         }
-        // 插入
+        // 插入链表第一个位置（头插法）
         Node n = new Node();
         n.key = k;
         n.val = v;
-        if (p.next != null) {
-            n.next = p.next;
-            p.next = n;
-        }
-        else {
-            // 如果是第一个元素或者是最后一个元素
-            p.next = p;
-            p.next = null;
-        }
+
+        n.next = head;
+        head = n;
         // 更新长度
         ++ length;
     }
