4bbd963386f6a1cb30edd3c1a6d39f79c485a201
bababadboy <sc30logo@gmail.com>
2019-03-03 14:58:55 +0800

    添加连通分量的测试用例

--- com/gakki/chapter4/p1_noDirectGraph/CC.java
+++ com/gakki/chapter4/p1_noDirectGraph/CC.java
@@ -1,13 +1,17 @@
 package com.gakki.chapter4.p1_noDirectGraph;
 
 import com.gakki.chapter4.Graph;
+import edu.princeton.cs.algs4.In;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Connected Component，dfs查找连同分量
  * @author wangxiaobin
  */
 public class CC {
-    private int[] id;   // 连通分量id
+    private int[] id;   // 连通分量id,[0...count-1]中可能
     private boolean[] marked;   // 是否被访问过
     private int count;      // 连通分量数
     /**
@@ -62,7 +66,47 @@ public class CC {
     }
 
     public static void main(String[] args) {
+        Graph G = new Graph(13);
+        G.addEdge(0,5);
+        G.addEdge(4,3);
+        G.addEdge(0,1);
+        G.addEdge(9,12);
+        G.addEdge(6,4);
+        G.addEdge(5,4);
+        G.addEdge(0,2);
+        G.addEdge(11,12);
+        G.addEdge(9,10);
+        G.addEdge(6,0);
+        G.addEdge(7,8);
+        G.addEdge(9,11);
+        G.addEdge(5,3);
 
+        CC cc = new CC(G);
+        int ccNum = cc.count(); // 连通分量数目，标记为:0...ccNum-1
+
+        /*  eg:[0,0,0,0,1,1,2,2,4,3,3,2,2]
+            第i个连通分量:components[i]
+         */
+        List<Integer>[] components = (List<Integer>[]) new List[ccNum];
+        // 把顶点归入属于自己的连通分量中
+        for (int i = 0; i < ccNum; i ++) {
+            int id = cc.id(ccNum);  // 获取连通分量标记：id
+            components[i] = new LinkedList<>();
+            for (int v = 0; v < G.V(); v ++) {
+                if (cc.id[v] == i){
+                    components[i].add(v);
+                }
+            }
+        }
+        // 输出连通分量数目
+        System.out.println(ccNum+" components");
+        // 输出各个连通分量
+        for (int i = 0 ; i < components.length; i ++) {
+            for (int w: components[i]) {
+                System.out.print(w+" ");
+            }
+            System.out.println();
+        }
     }
 
 }
