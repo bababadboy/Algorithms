d0565fb47775dfb20da90395cbad0e7f73f29168
bababadboy <sc30logo@gmail.com>
2019-03-12 20:24:22 +0800

    intern方法测试

--- com/gakki/basic/StringDemo.java
+++ com/gakki/basic/StringDemo.java
@@ -6,11 +6,18 @@ package com.gakki.basic;
  */
 public class StringDemo {
     public static void main(String[] args) {
-        String s2 = new String("Hello World"); // idea⚠️，不常用，会有性能问题
-        String s1 = "Hello World";
-        System.out.println(s1 == s2);       // false
-        System.out.println(s1.equals(s2));  // true
+//        String s2 = new String("Hello World"); // idea⚠️，不常用，会有性能问题
+//        String s1 = "Hello World";
+//        System.out.println(s1 == s2);       // false
+//        System.out.println(s1.equals(s2));  // true
 
+
+        String s1 = new String("aaa");
+        String s2 = new String("aaa");
+        System.out.println(s1 == s2);           // false
+        String s3 = s1.intern();
+        String s4 = s2.intern();
+        System.out.println(s3 == s4);           // true
     }
 
 }
