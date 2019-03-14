1e2dd960a15ca008ff3c1dc7f1e9b6a9f8948434
bababadboy <sc30logo@gmail.com>
2019-03-14 15:15:22 +0800

    实现了Iterable的的链表栈

---
+++ com/gakki/chapter1/Stack.java
@@ -0,0 +1,92 @@
+package com.gakki.chapter1;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+/**
+ * 链表实现栈
+ * @author wangxiaobin
+ */
+public class Stack<T> implements Iterable<T>{
+    class Node{
+        T item;
+        Node next;
+        public Node(T item){
+            this.item = item;
+        }
+    }
+
+    private Node first;     // 指向栈顶的指针
+    private int len;
+
+    public Stack(){
+        first = null;
+        len = 0;
+    }
+
+    public boolean isEmpty(){
+        return len == 0;
+    }
+
+    public void push(T item) {
+        Node n = new Node(item);
+        n.next = first;
+        first = n;
+        len ++;
+    }
+
+    public T pop(){
+        if (len == 0){
+            return null;
+        }
+        Node r = first;
+        first = r.next;
+        len --;
+        return r.item;
+
+    }
+
+    public T top(){
+        if (len == 0){
+            return null;
+        }
+        return first.item;
+    }
+
+    public int size(){
+        return len;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new ReverseIterator();
+    }
+    class ReverseIterator implements Iterator<T>{
+        Node p = first;
+        @Override
+        public boolean hasNext() {
+            return p != null;
+        }
+
+        @Override
+        public T next() {
+            T item = p.item;
+            p = p.next;
+            return item;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<String> stringStack = new Stack<>();
+        stringStack.push("Wxb");
+        stringStack.push("Wxb1");
+        stringStack.push("Wxb2");
+        for (String s:stringStack
+             ) {
+            System.out.print(s+"\t");
+        }
+//        System.out.println(stringStack.pop());
+//        System.out.println(stringStack.top());
+    }
+}
