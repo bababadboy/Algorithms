98c412001f69254712bb3448b9068d36bd5c092b
bababadboy <sc30logo@gmail.com>
2019-03-26 19:33:46 +0800

    重叠子问题存储优化

--- com/gakki/leetcode/DP/CountBits.java
+++ com/gakki/leetcode/DP/CountBits.java
@@ -5,21 +5,11 @@ package com.gakki.leetcode.DP;
  * @author wangxiaobin
  */
 public class CountBits {
-    public static void main(String[] args) {
-        int[] a = countBits(5);
-        for (int i = 0; i < a.length; i++) {
-            System.out.print(a[i]+'\t');
-        }
-
-        int[] b = countBitsFast(5);
-    }
-
     private static int[] countBitsFast(int num) {
         int[] store = new int[num+1]; // 0...n+1,重叠子问题优化，存储每个数字的转成2进制后1的个数
-        
+
         for(int i = 1; i <= num; i ++){
-            int n = i;
-            if (n % 2 == 1)
+            if (i % 2 == 1)
                 store[i] = store[i/2] + 1;
             else
                 store[i] = store[i/2];
@@ -43,5 +33,15 @@ public class CountBits {
             arr[i] = count;
         }
         return arr;
+    }
+
+    public static void main(String[] args) {
+
+        int[] b = countBitsFast(5);
+
+        for (int i = 0; i < b.length; i++) {
+            int a = b[i];
+            System.out.print(b[i]+"\t");
+        }
     }
 }
